493f97d62e1c15e3f85cc93081c74d47b5d6c46a
aimed-at-jupiter <201750005+aimed-at-jupiter@users.noreply.github.com>
2025-06-25 10:11:18 +0100

    creating blank project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header.jsx";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
